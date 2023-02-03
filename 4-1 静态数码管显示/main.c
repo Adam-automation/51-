4e7b633e7cd6d9787b8f136cd8a5a427f72cd690
Adam-automation <103024442+Adam-automation@users.noreply.github.com>
2023-02-04 00:18:27 +0800

    Add files via upload

---
+++ 4-1 静态数码管显示/main.c
@@ -0,0 +1,25 @@
+#include <at89c51RC2.h>
+unsigned char NixieTable[]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
+void NixieTube(unsigned char Location,Number)
+{
+	switch(Location)
+	{
+		case 1:P2_4=1;P2_3=1;P2_2=1;break;
+		case 2:P2_4=1;P2_3=1;P2_2=0;break;
+		case 3:P2_4=1;P2_3=0;P2_2=1;break;
+		case 4:P2_4=1;P2_3=0;P2_2=0;break;
+		case 5:P2_4=0;P2_3=1;P2_2=1;break;
+		case 6:P2_4=0;P2_3=1;P2_2=0;break;
+		case 7:P2_4=0;P2_3=0;P2_2=1;break;
+		case 8:P2_4=0;P2_3=0;P2_2=0;break;
+	}
+	P0=NixieTable[Number];
+}
+void main()
+{	
+	NixieTube(2,9);
+	while(1)
+	{
+		
+	}
+}
